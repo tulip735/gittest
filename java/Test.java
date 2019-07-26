8303748361afd6fad218b9e775b4e2aac0884e9d
blackTulip <ccd1035948164@gmail.com>
2019-07-26 21:27:20 +0800

    add java file

---
+++ java/Test.java
@@ -0,0 +1,25 @@
+//primitive
+
+
+import java.util.*;
+
+
+public class Test{
+
+
+	public static void main(String [] args){
+    
+//	    System.out.println(int.class.getName());
+//	    System.out.println(int.class.getCanonicalName());
+//	    System.out.println(int.class.getSimpleName());
+
+        System.out.println(int.class.getClass().toString());
+	    
+	    
+	    //class
+	    System.out.println(String.class.getName());
+	    System.out.println(String.class.getCanonicalName());
+	    System.out.println(String.class.getSimpleName());
+	}
+
+}
